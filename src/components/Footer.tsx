69f644f4a0cfe27546308a314478ef703a0aec57
Gerald Encabo <geraldencabo@gmail.com>
2023-05-30 19:19:45 -0400

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
                     <div className='footer-item-left'>
                         <ul>
                             <li className='footer-item-list'><a className='footer-social-media' title="Facebook" href='https://www.facebook.com/gerald.encabo.5/' target='_blank' rel='noreferrer'>Facebook</a></li>
-                            <li className='footer-item-list'><a className='footer-social-media' title="Instagram" href='https://www.instagram.com/imgeeerald/' target='_blank' rel='noreferrer'>Instagram</a></li>
+                            <li className='footer-item-list'><a className='footer-social-media' title="Instagram" href='https://www.instagram.com/iamgeerald/' target='_blank' rel='noreferrer'>Instagram</a></li>
                             <li className='footer-item-list'><a className='footer-social-media' title="LinkedIn" href='https://www.linkedin.com/in/gerald-encabo/' target='_blank' rel='noreferrer'>LinkedIn</a></li>
                             <li className='footer-item-list'><a className='footer-social-media' title="Github" href='https://github.com/gerald-encabo' target='_blank' rel='noreferrer'>Github</a></li>
                         </ul>
