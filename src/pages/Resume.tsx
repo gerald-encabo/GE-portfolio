37aa597e3484f056d3450c92e7a02d1a4d33d913
Gerald Encabo <geraldencabo@gmail.com>
2023-06-28 20:49:54 -0400

    Update Resume.tsx

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -35,40 +35,63 @@ export const Resume = () => {
                                             <h4 className='program'>Front-End UI Developer</h4>
                                             <p className='desc'>
                                                 <ul>
-                                                    <li>	Built and develop reusable components based on WCAG guidelines and UI design </li>
-                                                    <li>	Developed and maintain code in repositories to ensure code is centrally monitored and managed </li>
-                                                    <li>	Architected and implement component structure in Headless CMS Contentstack </li>
-                                                    <li>	Assisted back-end team in building User Interfaces components for clients </li>
-                                                    <li>	Works with QA tester to get new pages and components to be tested and address if any issues identified </li>
-                                                    <li>	Collaborated with stakeholders to achieve customer requirements and business goals </li>
-                                                    <li>	Converted mockup homepage design from Figma into prototype homepage that separated from different components </li>
-                                                    <li>	Conducted changes required to existing system (e.g., upgrade new version libraries, optimization) </li>
-                                                    <li>	Ensure user experience and feasibility determine design choices </li>
-                                                    <li>	Run extensive testing to ensure consistency, identify issues and improve UI across all devices and different browsers </li>
-                                                    <li>	Worked collaboratively in an agile environment with daily and weekly stand-ups along with the IT team and stakeholders </li>
-                                                    <li>	Used Monetate software for quick webpage updates primarily used for adding marketing content including monthly sales items, limited free shipping for specific products, etc. </li>
-                                                    <li>	Performed A/B testing using Adobe Target on the e-commerce site to maximize the user experience and increase conversion rates optimization </li>
-                                                    <li>	Task tracking assigned projects and UI tickets issues using Trello software </li>
-                                                    <li>	Participated in team code reviews for product release </li>
-                                                    <li>	Project Technology Stack: HTML5, CSS3, JavaScript (Vanilla & ES6), Bootstrap5, Sass, jQuery, React, JEST, JSON, Redux, ASP.NET (Razor), GitHub, Visual Studio, Figma, Monetate, Adobe Launch & Target, Contentstack (CMS) </li>
+                                                    <li> Build, designed, and developed reusable UI components using HTML5, CSS3, JavaScript (ES6), TypeScript, jQuery, Bootstrap5, and ReactJS that meet WCAG and AODA guidelines and web browsers standards for website </li>
+                                                    <li> Converted wireframe (Adobe XD) and mockup (Figma) homepage design into an interactive prototype which later deployed into the official homepage </li>
+                                                    <li> Applied optimization techniques to reduce page and image load times to enhance User Experience using Code-Splitting or Lazy loading </li>
+                                                    <li> Used Headless CMS ContentStack for maintaining components, templates, and assets </li>
+                                                    <li> Have worked on AWS for fetching pictures from AWS to the Image asset file </li>
+                                                    <li> Worked on Bootstrap, Media Queries, CSS Grid & Flexbox to build responsive design to provide a rich User Interface (UI) experience </li>
+                                                    <li> Implemented component design structure on CMS ContentStack and integrated with the client side using REST APIs </li>
+                                                    <li> Performed A/B testing using Adobe Target on the e-commerce site to maximize the user experience and increase conversion rates optimization </li>
+                                                    <li> Participated in team code reviews, and task tracking assigned projects and UI tickets issues using Trello software </li>
+                                                    <li> Used Monetate software for quick webpage updates used for adding marketing content such as monthly sales items, limited free shipping for specific products, etc. </li>
+                                                    <li> Updated product card component by adding CSS grid layout and styles for an efficient and responsive design for mobile and tablet view </li>
+                                                    <li> Worked closely with the back-end team to display data using the custom components, library components, and redux </li>
+                                                    <li> Implemented jQuery and React Slick carousel along with JavaScript libraries for the hero banner & carousel component </li>
+                                                    <li> Updated search engine input by adding an on-click clear button for a better overall User Experience </li>
+                                                    <li> Extensively used SASS (Syntactically Awesome Style Sheets) layout while styling with CSS3 </li>
+                                                    <li> Experience implementing the Model-View-Controller (MVC) architecture with ASP.NET, and Adobe Launch for tracking customer activity including search products by brand, category, price, etc. </li>
+                                                    <li> Worked on React Components, Forms, Events, Keys, Animations, Router, Icons, Bootstrap, Carousel & Redux concept </li>
+                                                    <li> Implemented masking credit card for payment transactions & administrator tester page for security purposes </li>
+                                                    <li> Extensively used Git for version controlling and regularly used Github for pushing and maintaining code in repositories </li>
+                                                    <li> Participated in day-to-day stand-up meetings along with the IT team and stakeholders to keep track of the project status </li>
+                                                    <li> Worked closely with the back-end team during product release to ensure smooth deployment in staging and production </li>
+                                                    <li> Performed extensive unit tests using JEST for all components to ensure consistency, identify issues, and improve UI across all devices and different browsers </li>
+                                                    <li> Technology used: HTML5, CSS3, JavaScript (ES6), TypeScript, Bootstrap5, SASS, jQuery, React, Redux, REST APIs, JEST, ASP.NET, GitHub, Visual Studio, Figma, Trello, AWS, Monetate, Adobe Launch, Contentstack (CMS)</li>
                                                 </ul>
                                             </p>
                                         </div>
                                         <div className='education-info-layer-three'>
                                             <p className='college'><span>2018 - 2022</span> Seneca College</p>
-                                            <h4 className='program'>Multi-Media Specialist & UI Web Developer</h4>
+                                            <h4 className='program'>AEM CQ5/Front-End UI Developer</h4>
                                             <p className='desc'>
                                                 <ul>
-                                                    <li>	Front-End experience in building, designing, and maintaining responsive web applications using CMS platform Adobe Experience Manager (AEM) </li>
-                                                    <li>	Work with Front-End technologies, components, and templates using modern technology </li>
-                                                    <li>	Led the homepage redesign project along with key stakeholders (UX designer and Marketing) </li>
-                                                    <li>	Followed the standard guideline of Web Content Accessibility Guideline (WCAG) and Accessibility for Ontarians with Disabilities Act (AODA) when creating and developing web pages </li>
-                                                    <li>	Performed quality assurance for code testing and accessibility checks using SiteImprove technology for fixing issues like bugs, broken links, typographical errors, and etc. </li>
-                                                    <li>	Adobe Photoshop is used for cropping and compressing the size of the image to improve web page performance specifically for mobile and tablet devices </li>
-                                                    <li>	Used Google technologies including Analytics for tracking website traffic, Maps API for displaying campus map, and Font API for importing new fonts style </li>
-                                                    <li>	Collaborated in Agile development team using Scrum methodologies when performing task assignment </li>
-                                                    <li>	Maintained and updated existing codes, and web content such as banner images, links, videos, and files attachment </li>
-                                                    <li>	Project Technology Stack: HTML5, CSS3, JavaScript (ES6), Bootstrap5, jQuery, Sass, React, Redux, ASP.NET, GitHub, Visual Studio, Figma, Adobe Experience Manager (CMS), OpenCMS, Adobe Target, Adobe Tagging, Adobe Photoshop, Adobe Analytics, Google Analytics, Gloo Maps, SiteImprove (Web Accessibility Checker & QA testing) </li>
+                                                    <li> Built and developed templates, dialogs, widgets, bundles, and components using modern front-end technologies like HTML5, CSS3, JavaScript, AJAX, jQuery, Bootstrap5, and React </li>     
+                                                    <li> Developed templates and components in Adobe CQ5 and Eclipse/CRXDE Lite </li>
+                                                    <li> Involved in content migration from OpenCMS to AEM 6.2 </li>
+                                                    <li> Administered components, templates, dialogs, and workflows in AEM </li>
+                                                    <li> Maintained Content Management System (CMS) and provided support for the application in authoring and publishing environments </li>
+                                                    <li> Collaborated with developers in migrating AEM 6.2 to 6.3 with a completely fresh code base using the latest technologies like React and SPA editor </li>
+                                                    <li> Designed & developed various web pages using CQ5, HTML5, CSS3, JavaScript, AJAX, jQuery, Bootstrap5 & React </li>
+                                                    <li> Familiar with Java, Servlets, JSP, XML and Apache Sling </li>
+                                                    <li> Extensively used CQ5 tagging features in developing components and templates </li>
+                                                    <li> Integrated AEM with marketing products tools like Adobe Tag, Target, Launch Data Layer, and Analytics </li>
+                                                    <li> Used Git as a distributed revision control system for CQ5 </li>
+                                                    <li> Involved Webpack as module bundler to preprocess and minify different files such as JavaScript and SASS files </li>
+                                                    <li> Implemented responsive web UI and made better user experience using SASS </li>
+                                                    <li> Implemented REST APIs on CRUD (create-read-update-delete) operations on assets and content using JSON and Postman for automation testing </li>
+                                                    <li> Conducted changes required to an existing system including upgrading new version plugin libraries for jQuery, Carousel, and Bootstrap </li>
+                                                    <li> Worked on landing pages and category landing pages using the content management system Adobe CQ5 for the consumer portal </li>
+                                                    <li> Adobe Photoshop is used for cropping and compressing the size of the image to improve web page performance specifically for mobile and tablet devices </li>
+                                                    <li> Followed the standard guideline of Web Content Accessibility Guideline (WCAG) (AA level) and Accessibility for Ontarians with Disabilities Act (AODA) when creating and developing web pages </li>
+                                                    <li> Performed quality assurance for code testing and accessibility checks using SiteImprove technology for fixing issues like bugs, broken links, typographical errors, etc. </li>
+                                                    <li> Maintained and update digital content (images, videos, files, etc.) on AEM DAM (Digital Asset Management) platform if its meeting business objectives and corporate standards </li>
+                                                    <li> Provided feedback to content creators/authors and ask for clarifications whenever there are questions before publishing </li>
+                                                    <li> Trained content creators/authors on how to use Adobe CQ5 to create, modify and publish content page </li>
+                                                    <li> Published content, and pages to the Seneca College production site environment </li>
+                                                    <li> Responsible for debugging, analyzing, and fixing all the code-related issues on the front end that arise out of the production systems </li>
+                                                    <li> Tested code changes in Development, QA, and Stage environments thoroughly before going to production </li>
+                                                    <li> Technology used: HTML5, CSS3, JavaScript (ES6), Bootstrap5, Ajax, jQuery, Sass, React, JSON, REST APIs, Agile (Scrum), Postman, Java, Servlets, JSP, XML, Apache Sling, Git, Visual Studio, Adobe Experience Manager (CMS), OpenCMS, Adobe (Target, Tag, Launch, Photoshop, Analytics) Google Analytics, SiteImprove (Web Accessibility & QA testing) </li> 
                                                 </ul>
                                             </p>
                                         </div>
@@ -78,20 +101,20 @@ export const Resume = () => {
                                             <p className='desc'>
                                             <ul>
                                                 <li> Created and updated web space and landing pages using build-in widgets in Intranet Software - Axero Solutions </li>
-                                                <li> Collaborated with clients about the structure of web space and page content for specific departments and faculties </li>
-                                                <li> Provided direct assistance and tutorial with the page administrator on developing and updating their respective web space and landing page </li>
-                                                <li> Maintained and updated existing web space and content including images, videos, links, file attachment, and marketing banners </li>
+                                                <li> Collaborated with clients about the structure of landing page, and page content for specific departments and faculties </li>
+                                                <li> Provided direct assistance and tutorials with the page administrators on developing and updating their respective intranet spaces </li>
+                                                <li> Maintained and updated existing web spaces and contents including images, videos, links, file attachment, and marketing banners </li>
                                                 <li> Track web performance and workflow using the build-in Analytics data feature in Axero Solutions Technology </li>
-                                                <li> Used of Front-End languages and frameworks such as HTML, CSS, JavaScript, and Bootstrap for webpage design, and layout </li>
+                                                <li> Used of Front-End languages and frameworks such as HTML, CSS, JavaScript, and Bootstrap for the intranet homepage and landing pages </li>
                                                 <li> Architected website structure templates using GlooMaps, and presented a UX design for the home page using Figma </li>
-                                                <li> Cropped and compressed the size of the image to improve web page performance, specifically for mobile and tablet devices </li>
+                                                <li> Cropped and compressed the size of the images to improve load page performance for mobile and tablet devices </li>
                                                 <li> Granted access accounts to students and employees on specific web spaces and pages </li>
                                                 <li> Performed extensive testing to ensure content consistency and responsiveness across all devices and different browsers </li>
-                                                <li> Managed the intranet workflow queue for content publishing to the different environments (develop, staging & production) </li>
+                                                <li> Managed the intranet workflow queue for content publishing to the different environments (staging & production) </li>
                                                 <li> Validated and review all content that before goes to the publishing stage or live environment </li>
-                                                <li> Reviewed digital content (content, images, videos, files, etc.) on the asset platform to validate if it meets objectives and standards </li>
+                                                <li> Reviewed digital content (files, images, videos, etc.) on the asset platform to validate if it meets objectives & standards </li>
                                                 <li> Provided feedback to content creators / authors and ask for clarifications whenever there are questions prior to publishing </li>
-                                                <li> Technology Stack: HTML5, CSS3, JavaScript (ES6), Bootstrap5, jQuery, JSON, GitHub, Visual Studio, Figma, Axero Solution Technology (CMS) </li>
+                                                <li> Technology used: HTML5, CSS3, JavaScript (ES6), Bootstrap5, jQuery, GitHub, Figma, GlooMaps, Axero Solution Technology (CMS) </li>
                                             </ul>
                                             </p>
                                         </div>
